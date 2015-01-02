9b4cb0df7c3a6b965b5824116df31657c5141a85
Josh Coody <joshua.n.coody@gmail.com>
2015-01-01 23:18:33 -0500

    added $.extend to the available functions

--- js/speed.js
+++ js/speed.js
@@ -19,6 +19,32 @@
 
   init.fn = init.prototype = $.fn;
 
+  $.extend = $.fn.extend = function(target) {
+    var isBool = typeof(target) === 'boolean' && target === true ? true : false;
+    target = isBool ? {} : target || {};
+    var key;
+    for(var i = 1; i < arguments.length; i++) {
+      if(isBool) {
+        var obj = arguments[i];
+        if(!obj) continue;
+        for(key in obj) {
+          if(obj.hasOwnProperty(key)) {
+            if(typeof(obj[key]) === 'object')
+              target[key] = $.extend({},target[key],obj[key]);
+            else
+              target[key] = obj[key];
+          }
+        }
+      }else{
+        if(!arguments[i]) continue;
+        for(key in arguments[i]) {
+          if(arguments[i].hasOwnProperty(key)) target[key] = arguments[i][key];
+        }
+      }
+    }
+    return target;
+  };
+
   $.fn.each = function(callback) {
     this.elements.forEach(function(element) {
       if(typeof(element)==='object') callback.call(element);
